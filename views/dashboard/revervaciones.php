9d76f2cfb2d92cafb07a8728047d80f0bcdabff3
WilliamOAC <williamamaya00729@gmail.com>
2021-06-27 20:25:25 -0600

    diseño dashboard

---
+++ views/dashboard/revervaciones.php
@@ -0,0 +1,9 @@
+<?php
+require_once("../../app/helpers/dashboard_template.php");
+Dashboard_Page::headerTemplate('Administrar reservaciones');
+?>
+
+<?php
+// Se imprime la plantilla del pie enviando el nombre del controlador para la página web.
+Dashboard_Page::footerTemplate('reservaciones.js');
+?>
